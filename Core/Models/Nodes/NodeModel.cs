a72ed7a48c2d5500e4be7cfba1fb1a517c7092d1
prasasdi <rizky.adie7@gmail.com>
2024-09-17 13:54:34 +0700

    mereplika dom tree

---
+++ Core/Models/Nodes/NodeModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models.Nodes
+{
+    public class NodeModel
+    {
+        public string Tag;
+        public string Text;
+        public NodeModel Parent;
+        public List<AttrModel> Attributes = new List<AttrModel>();
+        public List<NodeModel> Childrens = new List<NodeModel>();
+    }
+}
--- Core/Program.cs
+++ Core/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Core.Common;
 using Core.Helpers.Enums;
 using Core.Models;
+using Core.Models.Nodes;
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.Data.SqlTypes;
@@ -25,7 +26,7 @@ namespace Core
             string html1 = "<div ><span >dengan catatan:<span > ini adalah double span</span></span><br /><ul ><li >satu. dua dengan <i >italic <span >ini bisa jadi <b >bold</b></span></i></li><li >tiga dan empat dengan <b >bold</b></li></ul><p ><div ><span >maka dari itu</span> dengan ini saya sampaikan </div>kesimpulannya</p></div>\0";
             
             // Contoh dengan attr
-            string bar = "<div class=\"ini\">kan <div> dimana</div></div>\0";
+            string bar = "<div class=\"ini\" id=\"parent\" disabled>kan <div id=\"child\"> dimana</div></div>\0";
             string bar1 = "<div class=\"ini\">kan <div > dimana</div ></div >\0";
 
             string foo = "<div >kakaa</div>\0";
@@ -37,15 +38,75 @@ namespace Core
             {
                 html += '\0';
             }
-            Scanner scanner = Scanner.InitScanner(html);
+            Scanner scanner = Scanner.InitScanner(bar);
 
             Token token;
+            NodeModel node = new NodeModel();
+            AttrModel nodeAttr = new AttrModel();
             Console.WriteLine($"Strleng = {scanner.Length}");
             while (true)
             {
                 token = ScannerExtension.ScanToken(scanner);
                 // print token dimari
                 Console.WriteLine($"{token.Type} : {token.Value}");
+
+                switch (token.Type)
+                { 
+                    case TokenTypeEnum.TOKEN_TAG_START:
+                        // kalau tag kosong, asumsikan node adalah sebuah akar
+                        if (node.Tag == null)
+                        {
+                            node = new NodeModel()
+                            { 
+                                Tag = token.Value,
+                            };
+
+                        }
+                        else
+                        {
+                            // buat tempNode untuk dijadikan sebagai anak node
+                            var tempNode = new NodeModel()
+                            {
+                                Tag = token.Value,
+                                Parent = node
+                            };
+                            node.Childrens.Add(tempNode);
+
+                            // arahkan 'pointer' ke anak
+                            node = tempNode;
+                        }
+                        break;
+                    case TokenTypeEnum.TOKEN_TEXT:
+                        node.Text = token.Value;
+                        break;
+                    case TokenTypeEnum.TOKEN_ATTR_NAME:
+                        nodeAttr = new AttrModel()
+                        {
+                            Name = token.Value,
+                            Node = node
+                        };
+                        node.Attributes.Add(nodeAttr);
+                        // clear node attribute untuk attr selanjutnya
+                        nodeAttr = new AttrModel();
+                        break;
+                    case TokenTypeEnum.TOKEN_ATTR_VALUE:
+                        node.Attributes[node.Attributes.Count - 1].Value = token.Value;
+                        break;
+                    case TokenTypeEnum.TOKEN_SELF_CLOSING:
+                        // tambahkan sebagai child dari node
+                        node.Childrens.Add(new NodeModel()
+                        {
+                            Tag = token.Value,
+                            Parent = node
+                        });
+                        break;
+                    case TokenTypeEnum.TOKEN_TAG_END:
+                        if (node.Parent != null)
+                        {
+                            node = node.Parent;
+                        }
+                        break;
+                }
 
                 if (token.Type == TokenTypeEnum.TOKEN_EOF)
                 {
@@ -55,6 +116,11 @@ namespace Core
 
             scanner.FreeScanner();
             watch.Stop();
+            Console.WriteLine($"child node count: {node.Childrens.Count}");
+            foreach(var child in node.Childrens)
+            {
+                Console.WriteLine($"node child: {child.Tag} , parent node: {child.Parent.Tag}");
+            }
             Console.WriteLine();
             Console.WriteLine($"elapsed {watch.ElapsedMilliseconds} ms");
         }

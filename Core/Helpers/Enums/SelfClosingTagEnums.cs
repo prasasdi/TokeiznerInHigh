c2a3b7d460c0dbe6b026addbe41834fd1f9c7ccd
prasasdi <rizky.adie7@gmail.com>
2024-09-17 17:08:55 +0700

    tambah enum closing tags

---
+++ Core/Helpers/Enums/SelfClosingTagEnums.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Helpers.Enums
+{
+    public static class SelfClosingTagEnums
+    {
+        public static string[] Enums =
+        {
+        "area",
+        "base",
+        "br",
+        "col",
+        "embed",
+        "hr",
+        "img",
+        "input",
+        "link",
+        "meta",
+        "param",
+        "source",
+        "track",
+        "wbr",
+        };
+    }
+}
